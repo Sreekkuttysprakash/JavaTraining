1311379ad19618a324074331c9b07d5c43d2c77e
Sreekkutty <sreekkuttyappu26@gmail.com>
2023-06-16 09:07:44 +0530

    Second Commit

---
+++ Fibonacci.java
@@ -0,0 +1,21 @@
+class Fibonacci
+{
+public static void main(String args[])
+{
+int n1=1,n2=1,n3;
+System.out.println(n1);
+System.out.println(n2);
+{
+for(int i=3;i<=5;i++)
+{
+n3=n1+n2;
+System.out.println(n3);
+n1=n2;
+n2=n3;
+}
+}
+}
+}
+
+
+
